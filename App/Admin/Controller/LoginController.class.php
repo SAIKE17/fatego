a8193b34ca932bcb5d1d501f8d1ec60bd4a4867e
SAIKE17 <1059436959@QQ.COM>
2017-10-24 23:38:56 +0800

    完善登陆功能 添加layer插件

--- App/Admin/Controller/LoginController.class.php
+++ App/Admin/Controller/LoginController.class.php
@@ -11,12 +11,48 @@ use Think\Controller;
  */
 class LoginController extends Controller {
 
+    /**
+     * 登陆
+     */
+    public function login() {
+        if (IS_POST) {
+            
+            $a = I('post.');
+            
+            $this->error("登陆失败！");
+            $this->success("登陆成功！");
+        } else {
+            layout(false);
+            $this->display();
+        }
+    }
 
-    public function login(){
-        layout(false);
-        $this->display();
+    /**
+     * 获取验证码
+     */
+    public function captcha() {
+        // 验证码参数
+        $config = array(
+            'imageW' => 100,
+            'imageH' => 28,
+            'length' => 4,
+            'useNoise' => false,
+            'fontSize' => 14,
+            'bg' => array(255, 255, 255),
+        );
+
+        // 生成验证码
+        $verify = new \Think\Verify($config);
+        $verify->entry('fls');
+    }
+
+    /**
+     * 检测验证码
+     */
+    private function check_captcha($code, $id = 'fls') {
+        $verify = new \Think\Verify();
+
+        return $verify->check($code, $id);
     }
 
 }
-
-
